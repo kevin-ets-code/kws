3c1824d7b8f855e0cfaa28ce13eee949c19f3715
Kevin-doc <123083159+Kevin-doc@users.noreply.github.com>
2023-05-23 18:46:39 +0200

    Add files via upload

---
+++ change-color-background.js
@@ -0,0 +1,30 @@
+function changeBackgroundColorOnLoad() {
+  const sections = document.querySelectorAll('[id^="section-bg-"]');
+  const body = document.body;
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5 // Seuil de visibilité de la section
+  };
+
+  const sectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const section = entry.target;
+        const color = section.getAttribute('color');
+        if (color) {
+          body.style.transition = 'background-color 150ms ease';
+          body.style.backgroundColor = color;
+        }
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+}
+
+window.addEventListener('load', changeBackgroundColorOnLoad);
+changeBackgroundColorOnLoad();
