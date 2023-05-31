85b7b2186a081f5489effbe507f772ecddce5f66
@kevin <123083159+kevin-ets-code@users.noreply.github.com>
2023-05-31 10:52:28 +0200

    add shared-social-button essential.js

--- essential.js
+++ essential.js
@@ -1,7 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elementsToRemove = document.querySelectorAll('.w-condition-invisible');
-  
-  elementsToRemove.forEach(function(element) {
-    element.remove();
-  });
+function setupShareLinks() {
+    return new Promise(function(resolve, reject) {
+        let title = document.title;
+        let url = window.location.href;
+
+        const shareLinks = {
+            facebook: 'https://www.facebook.com/sharer/sharer.php?u=' + url + '%2F&title=' + title + '%3F',
+            linkedin: 'https://www.linkedin.com/shareArticle?mini=true&url=' + url + '%2F&title=' + title + '&summary=',
+            twitter: 'https://twitter.com/share?url=' + url + '%2F&title=' + title + '&summary=',
+            whatsapp: 'https://wa.me/?text=' + url
+        };
+
+        $('[shared-on]').each(function () {
+            let sharedValue = $(this).attr('shared-on');
+            let shareLink = shareLinks[sharedValue];
+
+            if (shareLink) {
+                $(this).attr('href', shareLink);
+                $(this).attr('target', '_blank');
+            }
+        });
+
+        resolve();
+    });
+}
+
+function removeInvisibleElements() {
+    return new Promise(function(resolve, reject) {
+        var elementsToRemove = document.querySelectorAll('.w-condition-invisible');
+
+        elementsToRemove.forEach(function(element) {
+            element.remove();
+        });
+
+        resolve();
+    });
+}
+
+$(document).ready(function () {
+    const functions = [
+      setupShareLinks, 
+      removeInvisibleElements
+    ];
+    const promises = functions.map(func => func());
+
+    Promise.all(promises);
 });
