c2c186a21f6a9de57b6a4100ff342a8b544b5939
@kevin <123083159+kevin-ets-code@users.noreply.github.com>
2023-06-10 15:26:29 +0200

    Add files via upload

---
+++ transform-to-button.js
@@ -0,0 +1,11 @@
+function replaceWithButton() {
+  var elements = document.querySelectorAll('.is-button');
+
+  elements.forEach(function(element) {
+    var button = document.createElement('button');
+    button.innerHTML = element.innerHTML;
+    button.className = element.className;
+    button.type = 'button';
+    element.replaceWith(button);
+  });
+}
