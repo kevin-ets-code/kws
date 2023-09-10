61646fdefad31d66bd7c836b3d0e89cb2cb65ec2
@kevin <123083159+kevin-ets-code@users.noreply.github.com>
2023-09-10 15:04:23 +0200

    Update change-visibility-with-select-element.js

--- change-visibility-with-select-element.js
+++ change-visibility-with-select-element.js
@@ -5,7 +5,7 @@ const elements = document.querySelectorAll('[select]');
 function handleChange() {
   const selectedValue = selectElement.value;
   elements.forEach(element => {
-    const elementValue = this.getAttribute('value');
+    const elementValue = this.getAttribute('select');
     if (elementValue === selectedValue) {
       element.style.display = 'block';
     } else {
