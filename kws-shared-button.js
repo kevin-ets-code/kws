8a2f2762cc48b03dd762e707e40e3670b34723a7
@kevin <123083159+kevin-ets-code@users.noreply.github.com>
2023-09-10 16:09:10 +0200

    Create kws-shared-button.js

---
+++ kws-shared-button.js
@@ -0,0 +1,22 @@
+var Webflow = Webflow || [];
+Webflow.push(function() {
+    let title = document.title;
+    let url = window.location.href;
+
+    const shareLinks = {
+        facebook: 'https://www.facebook.com/sharer/sharer.php?u=' + url + '%2F&title=' + title + '%3F',
+        linkedin: 'https://www.linkedin.com/shareArticle?mini=true&url=' + url + '%2F&title=' + title + '&summary=',
+        twitter: 'https://twitter.com/share?url=' + url + '%2F&title=' + title + '&summary=',
+        whatsapp: 'https://wa.me/?text=' + url
+    };
+
+    $('[shared-on]').each(function () {
+        let sharedValue = $(this).attr('kws-shared-on');
+        let shareLink = shareLinks[sharedValue];
+
+        if (shareLink) {
+            $(this).attr('href', shareLink);
+            $(this).attr('target', '_blank');
+        }
+    });
+});
